c78b2eafd0cc293a00b83655fe39d2d7bbd36d91
Ahmed Shehata <68691316+youngshehata@users.noreply.github.com>
2025-08-29 23:47:07 +0300

    feat: basic upload and sharping complete

--- src/common/abstracts/abstract.repo.ts
+++ src/common/abstracts/abstract.repo.ts
@@ -3,6 +3,9 @@ import {
   DEFAULT_PAGE_SIZE,
 } from '@common/constraints/constraints.common';
 import { HttpException } from '@nestjs/common';
+import * as fs from 'fs';
+import * as path from 'path';
+import * as sharp from 'sharp';
 
 export abstract class AbstractRepo<
   TDelegate extends {
@@ -10,11 +13,12 @@ export abstract class AbstractRepo<
     create: (...args: any[]) => any;
     update: (...args: any[]) => any;
     delete: (...args: any[]) => any;
+    findUnique: (...args: any[]) => any;
   },
 > {
   constructor(protected readonly delegate: TDelegate) {}
 
-  //! ===============================>   FindMany   <===============================
+  //! ===============================>   FIND MANY   <===============================
   findMany(
     args?: Parameters<TDelegate['findMany']>[0],
     pageSize: number = DEFAULT_PAGE_SIZE,
@@ -26,6 +30,20 @@ export abstract class AbstractRepo<
         take: Number(pageSize),
         skip: Number((pageNumber - 1) * pageSize),
       });
+    } catch (error) {
+      if (error.meta?.cause) {
+        throw new HttpException(error.meta.cause, 400);
+      }
+      throw error;
+    }
+  }
+
+  //! ===============================>   FIND ONE   <===============================
+  findOne(
+    args: Parameters<TDelegate['findUnique']>[0],
+  ): ReturnType<TDelegate['findUnique']> {
+    try {
+      return this.delegate.findUnique(args);
     } catch (error) {
       if (error.meta?.cause) {
         throw new HttpException(error.meta.cause, 400);
@@ -56,6 +74,84 @@ export abstract class AbstractRepo<
       const result = await this.delegate.update(args);
       return result;
     } catch (error) {
+      if (error.meta?.cause) {
+        throw new HttpException(error.meta.cause, 400);
+      }
+      throw error;
+    }
+  }
+
+  //! ===============================>   Update With Icon   <===============================
+
+  async updateWithIcon(
+    args: Parameters<TDelegate['update']>[0],
+    file?: Express.Multer.File,
+    type?: string,
+    imageSize?: 'icon' | 'image',
+  ): Promise<ReturnType<TDelegate['update']>> {
+    let filePath: string | null = null;
+    try {
+      if (!file) {
+        return await this.delegate.update(args);
+      }
+
+      if (!type) {
+        throw new HttpException('Type is required when uploading an icon', 400);
+      }
+
+      const uploadDir = path.join(process.cwd(), 'public', type);
+      if (!fs.existsSync(uploadDir)) {
+        fs.mkdirSync(uploadDir, { recursive: true });
+      }
+
+      const fileName = `${Date.now()}-${path.parse(file.originalname).name}.webp`;
+      filePath = path.join(uploadDir, fileName);
+
+      const fileBuffer = Buffer.from(file.buffer);
+
+      const processedImageBuffer = await sharp(fileBuffer)
+        .resize(
+          imageSize === 'icon' ? 64 : 800,
+          imageSize === 'icon' ? 64 : 600,
+          { fit: 'inside' },
+        )
+        .webp({
+          quality: imageSize === 'icon' ? 80 : 100,
+          alphaQuality: 100,
+          effort: 4,
+        })
+        .toBuffer();
+
+      await fs.promises.writeFile(filePath, processedImageBuffer);
+
+      const oldRecord = await this.delegate.findUnique({ where: args.where });
+
+      const updated = await this.delegate.update({
+        ...args,
+        data: {
+          ...args.data,
+          icon: `${fileName}`,
+        },
+      });
+
+      if (oldRecord?.icon) {
+        const oldIconPath = path.join(
+          process.cwd(),
+          'public',
+          type,
+          oldRecord.icon,
+        );
+        if (fs.existsSync(oldIconPath)) {
+          fs.unlinkSync(oldIconPath);
+        }
+      }
+
+      return updated;
+    } catch (error) {
+      // on error delete file if uploaded
+      if (filePath && fs.existsSync(filePath)) {
+        fs.unlinkSync(filePath);
+      }
       if (error.meta?.cause) {
         throw new HttpException(error.meta.cause, 400);
       }
--- src/features/skills/skills.controller.ts
+++ src/features/skills/skills.controller.ts
@@ -2,15 +2,19 @@ import {
   Body,
   Controller,
   Get,
-  HttpException,
   Param,
   Patch,
   Post,
+  Put,
   Query,
+  UploadedFile,
+  UseInterceptors,
 } from '@nestjs/common';
 import { SkillsService } from './skills.service';
 import { CreateSkillDto, UpdateSkillDto } from './dtos/skill.dto';
 import { PaginationFilter } from '@common/types/pagination.dto';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { multerValidations } from '@common/constraints/multer.options';
 
 @Controller('skills')
 export class SkillsController {
@@ -31,6 +35,33 @@ export class SkillsController {
   @Patch('/:id')
   async update(@Param('id') id: string, @Body() data: UpdateSkillDto) {
     const updated = await this.skillsService.update(+id, data);
+    return updated;
+  }
+  //! ============================================= EDIT with ICON =============================================
+  @Put('/icon/:id')
+  @UseInterceptors(FileInterceptor('file', multerValidations))
+  async updateIcon(
+    @Param('id') id: string,
+    @UploadedFile() file: Express.Multer.File,
+    @Body('type') type: string,
+    @Body('name') name: string,
+    @Body('showOnPortfolio') showOnPortfolio: boolean,
+  ) {
+    const data: Record<string, any> = {};
+    if (file) data.icon = file.filename;
+    if (name) data.name = name;
+    if (typeof showOnPortfolio !== 'undefined')
+      data.showOnPortfolio = showOnPortfolio;
+
+    const updated = await this.skillsService.updateWithIcon(
+      {
+        where: { id: Number(id) },
+        data,
+      },
+      file,
+      type,
+    );
+
     return updated;
   }
 }
