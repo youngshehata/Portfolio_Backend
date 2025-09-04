3ff88ea4dc0a20ea191da929e86622719356c71f
Ahmed Shehata <68691316+youngshehata@users.noreply.github.com>
2025-09-04 10:44:54 +0300

    fix: multer wrong file type throws

--- src/common/constraints/multer.options.ts
+++ src/common/constraints/multer.options.ts
@@ -1,3 +1,5 @@
+import { HttpException } from '@nestjs/common';
+
 const imagesAcceptedTypes = [
   'image/jpeg',
   'image/png',
@@ -10,25 +12,25 @@ const documentsAcceptedTypes = ['application/pdf'];
 export const multerValidations = {
   images: {
     limits: {
-      fileSize: 1024 * 1024 * 10, // 10MB
+      fileSize: 1024 * 1024 * (Number(process.env.IMAGES_MAX_SIZE_MB) || 10), // 10MB default
     },
     fileFilter: (_, file, callback) => {
       if (imagesAcceptedTypes.includes(file.mimetype)) {
         callback(null, true);
       } else {
-        callback(null, false);
+        callback(new HttpException('Invalid file type', 400), false);
       }
     },
   },
   documents: {
     limits: {
-      fileSize: 1024 * 1024 * 20, // MB
+      fileSize: 1024 * 1024 * (Number(process.env.DOCUMENTS_MAX_SIZE_MB) || 20), // 20MB default
     },
     fileFilter: (_, file, callback) => {
       if (documentsAcceptedTypes.includes(file.mimetype)) {
         callback(null, true);
       } else {
-        callback(null, false);
+        callback(new HttpException('Invalid file type', 400), false);
       }
     },
   },
